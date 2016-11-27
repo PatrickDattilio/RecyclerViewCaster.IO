a7792d0332677d30333be5b98777a94672478ee4
Dattilio, Patrick <Patrick.Dattilio@mlb.com>
2016-11-27 16:58:38 -0500

    Lesson 3, Horizontal list with reversed ordering.

--- app/src/main/java/io/caster/recyclerview/MainActivity.java
+++ app/src/main/java/io/caster/recyclerview/MainActivity.java
@@ -1,7 +1,7 @@
 package io.caster.recyclerview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,9 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true));
         recyclerView.setAdapter(new SimpleRVAdapter());
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL));
 
     }
 }
